da0dbf7501fc1a6626cd01046fed45967bd5dd0f
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-11-21 11:49:50 +0100

    void executeCode added

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _executeCode() {
+    int result;
+
+    for (int i = 10; i >= 0; i--) {
+      try {
+        result = 10 ~/ i;
+        print(result);
+      } catch (e) {
+        print('Fehler aufgetreten: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +51,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Animated Font Size'),
       ),
       body: Center(
-        child: AnimatedDefaultTextStyle(
-          style: TextStyle(fontSize: _fontSize, color: Colors.black),
-          duration: const Duration(seconds: 1),
-          child: Text('Hello, App Akademie!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedDefaultTextStyle(
+              style: TextStyle(fontSize: _fontSize, color: Colors.black),
+              duration: const Duration(seconds: 1),
+              child: Text('Hello, App Akademie!'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _executeCode();
+              },
+              child: Text('Code ausführen'),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
